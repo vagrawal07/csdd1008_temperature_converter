50e1d19e8a6e4e74b0413788f6a240167c5af902
VeerpalKaur <veerpalkaur20@loyalistcollege.com>
2024-05-30 19:52:32 -0400

    Added formula of temperature conversion

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+// FahrenheitToCelsius converts a Fahrenheit temperature to Celsius.
+func FahrenheitToCelsius(f float64) float64 {
+	return (f - 32) * 5 / 9
+}
+
+// CelsiusToFahrenheit converts a Celsius temperature to Fahrenheit.
+func CelsiusToFahrenheit(celsius float64) float64 {
+	return celsius*9/5 + 32
+}u
+
 func main() {
 	fmt.Println("Created Project Structure")
 }
